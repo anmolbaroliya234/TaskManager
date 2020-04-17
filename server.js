43a88fc5525b7e13702fff97dabe61a0f59b74c2
anmolbaroliya234 <63810409+anmolbaroliya234@users.noreply.github.com>
2020-04-17 17:23:46 +0530

    Update port envt

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const todoRoute = require('./route/todo')
 const todoOrderby = require('./route/order')
 const app = express()
 
-const server_port=process.env.PORT||3333
-
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
@@ -18,7 +16,7 @@ app.use('/orderby',todoOrderby)
 
 db.sync()
   .then(() => {
-    app.listen(server_port)
+    app.listen(process.env.PORT || 6565)
     console.log("server started");
   })
   .catch((err) => {
